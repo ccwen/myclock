41638b797638485ed20e57dde9004f0e7a407948
yapcheahshen <yapcheahshen@gmail.com>
2015-02-09 16:08:59 +0800

    my clock

---
+++ ksana.js
@@ -0,0 +1,40 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "myclock",
+ "minruntime": 1,
+ "baseurl": "http://yapcheahshen@rawgit.com/yapcheahshen/myclock/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  197,
+  64,
+  155651,
+  277980,
+  645243,
+  742,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-02-09T08:08:35.119Z",
+  "2015-02-09T08:06:49.992Z",
+  "2015-02-09T08:08:37.291Z",
+  "2015-02-09T08:06:50.023Z",
+  "2015-02-09T08:06:50.054Z",
+  "2015-02-09T08:07:54.173Z",
+  "2015-02-09T08:06:49.992Z",
+  "2015-02-09T08:06:49.976Z"
+ ],
+ "date": "2015-02-09T08:08:38.272Z"
+})
